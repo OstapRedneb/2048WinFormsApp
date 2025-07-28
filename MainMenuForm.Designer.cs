c275fc01822437fca418991fe1972ef343ef8200
OstapRedneb <rodionzabirov888@gmail.com>
2025-07-28 18:33:00 +0500

    Сделал функционал главного меню

--- MainMenuForm.Designer.cs
+++ MainMenuForm.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenuForm));
+            startButton = new Button();
+            resultsButton = new Button();
+            settingsButton = new Button();
+            exitButton = new Button();
+            SuspendLayout();
+            // 
+            // startButton
+            // 
+            startButton.BackColor = Color.FromArgb(64, 0, 64);
+            startButton.Font = new Font("Comic Sans MS", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            startButton.ForeColor = Color.RoyalBlue;
+            startButton.Location = new Point(258, 12);
+            startButton.Name = "startButton";
+            startButton.Size = new Size(175, 85);
+            startButton.TabIndex = 0;
+            startButton.Text = "Начать";
+            startButton.UseVisualStyleBackColor = false;
+            startButton.Click += startButton_Click;
+            // 
+            // resultsButton
+            // 
+            resultsButton.BackColor = Color.FromArgb(64, 0, 64);
+            resultsButton.Font = new Font("Comic Sans MS", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            resultsButton.ForeColor = Color.RoyalBlue;
+            resultsButton.Location = new Point(80, 12);
+            resultsButton.Name = "resultsButton";
+            resultsButton.Size = new Size(175, 85);
+            resultsButton.TabIndex = 1;
+            resultsButton.Text = "Результаты";
+            resultsButton.UseVisualStyleBackColor = false;
+            resultsButton.Click += resultsButton_Click;
+            // 
+            // settingsButton
+            // 
+            settingsButton.BackColor = Color.FromArgb(64, 0, 64);
+            settingsButton.Font = new Font("Comic Sans MS", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            settingsButton.ForeColor = Color.RoyalBlue;
+            settingsButton.Location = new Point(436, 12);
+            settingsButton.Name = "settingsButton";
+            settingsButton.Size = new Size(175, 85);
+            settingsButton.TabIndex = 2;
+            settingsButton.Text = "Настройки";
+            settingsButton.UseVisualStyleBackColor = false;
+            settingsButton.Click += settingsButton_Click;
+            // 
+            // exitButton
+            // 
+            exitButton.BackColor = Color.FromArgb(64, 0, 64);
+            exitButton.Font = new Font("Comic Sans MS", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            exitButton.ForeColor = Color.Red;
+            exitButton.Location = new Point(262, 475);
+            exitButton.Name = "exitButton";
+            exitButton.Size = new Size(171, 85);
+            exitButton.TabIndex = 3;
+            exitButton.Text = "Выход";
+            exitButton.UseVisualStyleBackColor = false;
+            exitButton.Click += exitButton_Click;
+            // 
+            // MainMenuForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(681, 563);
+            Controls.Add(exitButton);
+            Controls.Add(settingsButton);
+            Controls.Add(resultsButton);
+            Controls.Add(startButton);
+            DoubleBuffered = true;
+            Name = "MainMenuForm";
+            Text = "Игра 2048";
+            Load += MainMenuForm_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button startButton;
+        private Button resultsButton;
+        private Button settingsButton;
+        private Button exitButton;
     }
 }
--- MainMenuForm.cs
+++ MainMenuForm.cs
@@ -6,5 +6,29 @@ namespace _2048WinFormsApp
         {
             InitializeComponent();
         }
+        public void MainMenuForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void startButton_Click(object sender, EventArgs e)
+        {
+            new GameForm().Show();
+            this.Close();
+        }
+        public void resultsButton_Click(object sender, EventArgs e)
+        {
+            new ResultsForm().Show();
+            this.Close();
+        }
+        public void settingsButton_Click(object sender, EventArgs e)
+        {
+            new SettingsForm().Show();
+            this.Close();
+        }
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
