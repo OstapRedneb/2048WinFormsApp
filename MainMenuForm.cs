5c08cb6b50a95c07a0c624e2dbe65443c8543a22
OstapRedneb <rodionzabirov888@gmail.com>
2025-07-28 18:02:56 +0500

    Добавьте файлы проекта.

---
+++ MainMenuForm.cs
@@ -0,0 +1,10 @@
+namespace _2048WinFormsApp
+{
+    public partial class MainMenuForm : Form
+    {
+        public MainMenuForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
